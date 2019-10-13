921c4e2bf5ab7be5bf738c72b4514fb57d6cf26d
Pia Soy <piasoy4@gmail.com>
2019-10-13 09:48:07 -0500

    make homepage.mustache template, add route to render, installed mustache

--- server.js
+++ server.js
@@ -2,9 +2,26 @@ const dbConfigs = require('./knexfile.js')
 const db = require('knex')(dbConfigs.development)
 const express = require('express')
 const app = express();
-
 const port = 3000; 
 
+
+
+const fs = require('fs')
+const mustache = require('mustache')
+const homepageTemplate = fs.readFileSync('./templates/homepage.mustache', 'utf8')
+
+//home page
+app.get('/', function (req, res) { 
+  res.send(mustache.render(homepageTemplate))
+})
+
+
+
+
+
+
+
+
 app.listen(port, function () {
-    console.log('Listening on port ' + port + ' 👍')
-  })
+  console.log('Listening on port ' + port + ' 👍')
+})
